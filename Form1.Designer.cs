fadd30390b8e7df9f43c365b365a19ab773036cd
salavat14 <41482996+salavat14@users.noreply.github.com>
2018-07-20 20:45:52 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace MusicDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.findBtn = new System.Windows.Forms.Button();
+            this.requestTB = new System.Windows.Forms.TextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.tabControl2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(758, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Статистика";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(758, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.ItemSize = new System.Drawing.Size(0, 1);
+            this.tabControl2.Location = new System.Drawing.Point(12, 12);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(740, 543);
+            this.tabControl2.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl2.TabIndex = 2;
+            this.tabControl2.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.groupBox2);
+            this.tabPage3.Controls.Add(this.groupBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 5);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(732, 534);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 66);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(713, 494);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Результаты поиска";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AcceptsTab = true;
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(7, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(700, 427);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.findBtn);
+            this.groupBox1.Controls.Add(this.requestTB);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(713, 54);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Поиск";
+            // 
+            // findBtn
+            // 
+            this.findBtn.Location = new System.Drawing.Point(632, 19);
+            this.findBtn.Name = "findBtn";
+            this.findBtn.Size = new System.Drawing.Size(75, 23);
+            this.findBtn.TabIndex = 1;
+            this.findBtn.Text = "Найти";
+            this.findBtn.UseVisualStyleBackColor = true;
+            this.findBtn.Click += new System.EventHandler(this.findBtn_Click_1);
+            // 
+            // requestTB
+            // 
+            this.requestTB.Location = new System.Drawing.Point(7, 20);
+            this.requestTB.Name = "requestTB";
+            this.requestTB.Size = new System.Drawing.Size(619, 20);
+            this.requestTB.TabIndex = 0;
+            this.requestTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.requestTB_KeyPress_1);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.richTextBox2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 5);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(732, 534);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "tabPage2";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(719, 502);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(759, 39);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 4;
+            this.backButton.Text = "Назад";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Visible = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 567);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MusicDownloader";
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button findBtn;
+        private System.Windows.Forms.TextBox requestTB;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button backButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Fizzler.Systems.HtmlAgilityPack;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace MusicDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void search(string objectOfSearch)
+        {
+            richTextBox1.Invoke(new Action<string>((s) => richTextBox1.Text = s), "");
+            int quanOfPages = 1;
+            for (int page = 1; page <= quanOfPages; page++)
+            {
+                string link = getUrl(objectOfSearch, page);
+                string htmlCode = getHtml(link);
+                string textInRTb = "";
+                List<int> noLinksSongs = new List<int>();
+                string[] song = getSongsOnPage(link);
+                for (int i = 0; i < song.Length; i++)
+                {
+                    textInRTb += song[i] + "\n";
+                }
+                List<string> HREFS = getHREFtoPages(htmlCode);
+                HREFS = sortHrefs(HREFS);
+                while (HREFS.Count > song.Length)
+                    HREFS.RemoveAt(HREFS.Count - 1);
+                for (int i = 0; i < song.Length; i++)
+                {
+                    richTextBox1.Text += song[i] + " " + HREFS[i] + "\n";
+                }
+                if (song.Length == 0) richTextBox1.Text = "Песни с таким названием не найдено.";
+                quanOfPages = getQuanOfPages(link);
+            }
+        }
+
+        public string getUrl(string objectOfSearch, int page)
+        {
+            string link = "";
+            link = "http://zaycev.net/search.html?page=" + page + "&query_search=";
+            link += objectOfSearch;
+            return link;
+        }
+        public List<string> getHREFtoPages(string htmlCode)
+        {
+            string[] htmlString = htmlCode.Split('\n');
+            List<string> HREFS = new List<string>();
+            string buf = "";
+            for (int j = 0; j < htmlString.Length; j++)
+            {
+                for (int i = 0; i < htmlString[j].Length - 12; i++)
+                {
+                    if (htmlString[j].Substring(i, 12) == "href=\"/pages")
+                    {
+                        buf = "";
+                        i += 6;
+                        while (htmlString[j].Substring(i, 1) != @"""")
+                        {
+                            buf += htmlString[j].Substring(i, 1);
+                            i++;
+                        }
+                        buf = "http://zaycev.net" + buf;
+                        HREFS.Add(buf);
+                    }
+                }
+            }
+            return HREFS;
+        }
+        public List<string> sortHrefs(List<string> HREFS)
+        {
+            List<string> sortingHREFS = new List<string>();
+            for (int i = 0; i < HREFS.Count; i++)
+            {
+                for (int j = 0; j < HREFS.Count; j++)
+                {
+                    if ((HREFS[i] == HREFS[j]) && (i != j))
+                        HREFS[j] = null;
+                }
+                if (HREFS[i] != null)
+                    sortingHREFS.Add(HREFS[i]);
+            }
+            return sortingHREFS;
+        }
+        public string[] editTime(string[] song)
+        {
+            for (int i = 0; i < song.Length; i++)
+            {
+                for (int j = 0; j < song[i].Length; j++)
+                {
+                    if (song[i][j] == ':')
+                    {
+                        string songLength = song[i].Remove(0, j - 2);
+                        song[i] = song[i].Remove(j - 2);
+                        song[i] += "(" + songLength + ")";
+                        j += 2;
+                    }
+                }
+            }
+            return song;
+        }
+        public string[] getSongsOnPage(string link)
+        {
+            string htmlCode = getHtml(link);
+            string text = "";
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(htmlCode);
+            foreach (var item in doc.DocumentNode.QuerySelectorAll("div.search-page__tracks"))
+            {
+                text += item.InnerText;
+            }
+            string[] lines = text.Split('\n');
+            string[] song = new string[lines.Length - 1];
+            for (int i = 0; i < song.Length; i++)
+                song[i] = lines[i + 1];
+            song = editTime(song);
+            return song;
+        }
+        public int getQuanOfPages(string link)
+        {
+            string htmlCode = getHtml(link);
+            string text = "";
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(htmlCode);
+            foreach (var item in doc.DocumentNode.QuerySelectorAll("div.search-pager-container"))
+            {
+                text += item.InnerText;
+            }
+            if (text != "") //если не одна страница
+            {
+                if (text.Length > 9)
+                    text = text.Remove(text.Length - 9); //если это не последняя страница
+                int lastPage = Convert.ToInt32(text.Remove(0, text.Length - 1));
+                return lastPage;
+            }
+            else return 1;
+        }
+        public string getHtml(string link)
+        {
+            string htmlCode;
+            WebClient client = new WebClient();
+            client.DownloadFile(link, "test.html");
+            htmlCode = File.ReadAllText("test.html");
+            return htmlCode;
+        }
+        public string getHREFtoSave(string htmlCode)
+        {
+            string[] htmlString = htmlCode.Split('\n');
+            string buf = "";
+            for (int j = 0; j < htmlString.Length; j++)
+            {
+                for (int i = 0; i < htmlString[j].Length - 18; i++)
+                {
+                    if (htmlString[j].Substring(i, 18) == "href=\"http://cdndl")
+                    {
+                        buf = "";
+                        i += 6;
+                        while (htmlString[j].Substring(i, 1) != @"""")
+                        {
+                            buf += htmlString[j].Substring(i, 1);
+                            i++;
+                        }
+                    }
+                }
+            }
+            return buf;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tabControl2.SelectedIndex == 0)
+            {
+                tabControl2.SelectedIndex = 1;
+                printStat();
+                backButton.Visible = true;
+            }
+            else
+            {
+                tabControl2.SelectedIndex = 0;
+                backButton.Visible = false;
+            }
+        }
+        public void printStat()
+        {
+            richTextBox2.Text = "";
+            string connectionString = ConfigurationManager.ConnectionStrings["MusicDownloader.Properties.Settings.userdbConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sqlExpression = "SELECT * FROM Songs";
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                List<string> name = new List<string>();
+                while (reader.Read())
+                {
+                    name.Add(reader["name"].ToString());
+                }
+                string[] masOfPerf = name.ToArray();
+                for (int i = 0; i < masOfPerf.Length; i++)
+                {
+                    if (masOfPerf[i] != "")
+                    {
+                        richTextBox2.Text += masOfPerf[i];
+                        int qualOfRep = 1;
+                        for (int j = 0; j < masOfPerf.Length; j++)
+                        {
+                            if ((masOfPerf[j] != "") && (masOfPerf[i] == masOfPerf[j]) && (i != j))
+                            {
+                                qualOfRep++;
+                                masOfPerf[j] = "";
+                            }
+                        }
+                        richTextBox2.Text += String.Format("[{0}] \n", qualOfRep.ToString());
+                    }
+                }
+            }
+        }
+        private void findBtn_Click_1(object sender, EventArgs e)
+        {
+            search(requestTB.Text);
+        }
+
+        private void requestTB_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                findBtn.PerformClick();
+            }
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MusicDownloader.Properties.Settings.userdbConnectionString"].ConnectionString;
+            string htmlCode = getHtml(e.LinkText);
+            string link = getHREFtoSave(htmlCode);
+            int lenthOfName = 0, beginOfName = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                int endOfName = richTextBox1.Text.IndexOf(e.LinkText) - 3; // begin of link - 3
+                for (int i = 0; i < 1000; i++) //считываем название по одному символу с конца
+                {
+
+                    if (endOfName - i == 0) // если стоит первым и дошли до начала строки
+                    {
+                        lenthOfName = i;
+                        beginOfName = 0;
+                        break;
+                    }
+                    else if (richTextBox1.Text.Substring(endOfName - i, 1) == "\n")
+                    {
+                        lenthOfName = i - 1;
+                        beginOfName = endOfName - i + 1;
+                        break;
+                    }
+                }
+                string name = richTextBox1.Text.Substring(beginOfName, lenthOfName - 7);
+                string[] performAndName = name.Split(Convert.ToChar("–"));
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO Songs (Name, DateTime) VALUES (@name, @datetime)";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@name", performAndName[0]);
+                cmd.Parameters.AddWithValue("@datetime", System.DateTimeOffset.Now);
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+            }
+            try
+            {
+                System.Diagnostics.Process.Start(link);
+            }
+            catch (Exception InvalidOperationException)
+            {
+                MessageBox.Show(
+                    "Ссылка на скачивание отсутствует",
+                    "Ошибка!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1
+                    );
+            }
+        }
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            tabControl2.SelectedIndex = 0;
+            backButton.Visible = false;
+        }
+    }
+}
